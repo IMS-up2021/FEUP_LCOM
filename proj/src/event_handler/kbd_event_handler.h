9f9d13e38caef553fd9b3a2eedac10278dc742b8
André dos Santos Faria Relva <up202108695@fe.up.pt>
2024-06-02 20:47:20 +0100

    Fully compiles, properly drawing

--- proj/src/event_handler/kbd_event_handler.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "drivers/kbd.h"
-#include "game/game_state.h"
-
-void handle_kbd_event(uint8_t scan_code[2]);
-void handle_kbd_game_event(uint8_t scan_code[2]);
-void handle_kbd_leaderboard_event(uint8_t scan_code[2]);
-void handle_kbd_instructions_event(uint8_t scan_code[2]);
-
--- proj/src/event_handler/mouse_event_handler.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "drivers/mouse.h"
-#include "game/game_state.h"
-
-void handle_mouse_event(struct packet pp);
-void handle_mouse_game_event(struct packet pp);
-void handle_mouse_menu_event(struct packet pp);
-void handle_mouse_leaderboard_event(struct packet pp);
-void handle_mouse_instructions_event(struct packet pp);
